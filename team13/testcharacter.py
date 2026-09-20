4a8ba261f92e7d579351f0c0e3b600494dde9b82
SylviaTheFox <jstrayer@wpi.edu>
2026-09-20 13:36:16 -0400

    Heuristic Done

--- team13/testcharacter.py
+++ team13/testcharacter.py
@@ -12,6 +12,47 @@ class TestCharacter(CharacterEntity):
         pass
     
     def createHeuristic(self, wrld):
+        heuristics = [[0]*wrld.width()]*wrld.height()
+        # monster
+        for x in range(wrld.width()):
+            for y in range(wrld.height()):
+                if wrld.monster_at(x,y):
+                    heuristics[y][x] += 100
+                    if y > 0:
+                        heuristics[y-1][x] += 50
+                        if x > 0:
+                            heuristics[y-1][x-1] += 15
+                        if x < wrld.width() - 1:
+                            heuristics[y-1][x+1] += 15
+                    if x > 0:
+                        heuristics[y][x-1] += 50
+                    if y < wrld.height() - 1:
+                        heuristics[y+1][x] += 50
+                        if x > 0:
+                            heuristics[y+1][x-1] += 15
+                        if x < wrld.width() - 1:
+                            heuristics[y+1][x+1] += 15
+                    if x < wrld.width() - 1:
+                        heuristics[y][x+1] += 50
+                    if y > 1:
+                        heuristics[y-2][x] += 15
+                    if x > 1:
+                        heuristics[y][x-2] += 15
+                    if y < wrld.height() - 2:
+                        heuristics[y+2][x] += 15
+                    if x < wrld.width() - 2:
+                        heuristics[y][x+2] += 15
+                    
+        
+        # wall
+        for x in range(wrld.width()):
+            for y in range(wrld.height()):
+                if wrld.wall_at(x, y):
+                    heuristics[y][x] = 100
+                
+        pass
+    
+    def calcMoveAstar(self, wrld, heur):
         pass
     
     
